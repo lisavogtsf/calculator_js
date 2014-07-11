e2061efc0bf378b624012b5c9682d755418103ad
Lisa Vogt <lisavogt.sf@gmail.com>
2014-07-10 21:59:45 -0700

    numbers displaying

--- calculator.js
+++ calculator.js
@@ -2,10 +2,71 @@
 window.onload = function() {
 	var one = document.getElementById("oneButton");
 	var two = document.getElementById("twoButton");
-
+	var three = document.getElementById("threeButton");
+	var four = document.getElementById("fourButton");
+	var five = document.getElementById("fiveButton");
+	var six = document.getElementById("sixButton");
+	var seven = document.getElementById("sevenButton");
+	var eight = document.getElementById("eightButton");
+	var nine = document.getElementById("nineButton");			
+	var zero = document.getElementById("zeroButton");
+	var opDisp = document.getElementById("operator_display");
 
 
 	one.onclick = function(event) {
-		alert("button one");
+		console.log("oneButton");
+		opDisp.innerHTML += one.innerHTML;
+
 	}
+	two.onclick = function(event) {
+		console.log("twoButton");
+		opDisp.innerHTML += two.innerHTML;
+
+	}
+	// three.onclick = function(event) {
+	// console.log("threeButton");
+
+	// }
+	four.onclick = function(event) {
+		console.log("fourButton");
+		opDisp.innerHTML += four.innerHTML;
+	}
+	// five.onclick = function(event) {
+	// console.log("fiveButton");
+
+	// }
+	// six.onclick = function(event) {
+	// console.log("sixButton");
+
+	// }
+	// seven.onclick = function(event) {
+	// console.log("sevenButton");
+
+	// }
+	// eight.onclick = function(event) {
+	// console.log("eightButton");
+
+	
+	// }
+	// nine.onclick = function(event) {
+	// console.log("nineButton");
+
+	
+	// }
+	// zero.onclick = function(event) {
+	// 	console.log("zeroButton");
+		
+	// }
+
+
+
+
+
+
+
+
+
+
+
+
 }
