92d065215583a428a0ecbd654247b90b5abf26b8
Lisa Vogt <lisavogt.sf@gmail.com>
2014-07-10 17:35:09 -0700

    some js running

---
+++ calculator.js
@@ -0,0 +1,11 @@
+// alert("javascript is working");
+window.onload = function() {
+	var one = document.getElementById("oneButton");
+	var two = document.getElementById("twoButton");
+
+
+
+	one.onclick = function(event) {
+		alert("button one");
+	}
+}
